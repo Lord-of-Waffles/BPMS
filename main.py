1163d83f7e3c470d7fa3fed2f64bcbb66252cf0b
Lord-of-Waffles <ben.worton@gmail.com>
2025-11-03 14:12:13 +0100

    added merge-config task, made the process work from start to finish

--- main.py
+++ main.py
@@ -104,43 +104,85 @@ def run_worker():
                 
     @worker.task(task_type="send-progress-update")
     def send_progress_update(json_data: dict):
-        return {"updateText": f"Your VM called {json_data["VM Name"]} was configured correctly, and is currently being initialised. This process can take up to 10 minutes. Please be patient as we get it ready for you!"}
-    
+        return {"updateText": f"Your VM called {json_data['VM Name']} was configured correctly, and is currently being initialised. This process can take up to 10 minutes. Please be patient as we get it ready for you!"}    
     @worker.task(task_type="receive-progress-update")
     def receive_progress_update(updateText: str):
         print(updateText)
-
+        return {}
 
     @worker.task(task_type="send-vm-config")
-    def send_vm_config():
+    def send_vm_config(VMRequestID):
+        print(f"Sending VM Configuration with ID: {VMRequestID} to Data Layer")
         return {}
 
     @worker.task(task_type="receive-vm-config")
-    def receive_vm_config():
+    def receive_vm_config(json_data: dict, VMRequestID):
+        print(f"Received VM Request ID: {VMRequestID} from App Layer with this config: {json_data}")
         return {}
     
     @worker.task(task_type="start-initialisation")
     def start_initialisation():
+        print("-=====================-")
+        print("Initialising VM...")
         return {}
 
     @worker.task(task_type="allocate-ram")
-    def allocate_ram():
-        return {}
+    def allocate_ram(json_data: dict):
+        print("Initialising RAM...")
+    
+        if json_data["VM Size"] == "smallVM":
+            vm_ram = "2GB"
+        elif json_data["VM Size"] == "mediumVM":
+            vm_ram = "4GB"
+        else:
+            vm_ram = "8GB"
+    
+        return {"vm_ram": vm_ram}
 
     @worker.task(task_type="processing-power")
-    def processing_power():
-        return {}
+    def processing_power(json_data: dict):
+        print("Initialising vCPU...")
+    
+        if json_data["VM Size"] == "smallVM":
+            vm_vcpu = "2 Cores"
+        elif json_data["VM Size"] == "mediumVM":
+            vm_vcpu = "4 Cores"
+        else:
+            vm_vcpu = "8 Cores"
+    
+        return {"vm_vcpu": vm_vcpu}
 
     @worker.task(task_type="allocate-storage")
-    def allocate_storage():
-        return {}
+    def allocate_storage(json_data: dict):
+        print("Initialising VM Storage...")
+    
+        if json_data["VM Size"] == "smallVM":
+            vm_storage = "10 GB"
+        elif json_data["VM Size"] == "mediumVM":
+            vm_storage = "20 GB"
+        else:
+            vm_storage = "30 GB"
+    
+        return {"vm_storage": vm_storage}
 
+    @worker.task(task_type="merge-config")
+    def merge_config(json_data: dict, vm_ram: str, vm_vcpu: str, vm_storage: str):
+        print("Merging configuration from parallel tasks...")
+        updated_data = dict(json_data)
+        updated_data["VM Ram"] = vm_ram
+        updated_data["VM vCPU"] = vm_vcpu
+        updated_data["VM Storage"] = vm_storage
+    
+        return {"json_data": updated_data}
+    
     @worker.task(task_type="send-result")
     def send_result():
-        return 
-    {}
+        print("VM Initialisation complete! Sending result...")
+        return {}
+
     @worker.task(task_type="receive-result")
-    def receive_result():
+    def receive_result(json_data: dict):
+        print(f"\nInitialisation process completed. VM called {json_data['VM Name']} has been initialised with \n{json_data['VM Ram']} of RAM,\na vCPU with {json_data['VM vCPU']} \nand {json_data['VM Storage']} of storage.")
         return {}
     print("Zeebe worker started")
     zeebe_loop.run_until_complete(worker.work())
